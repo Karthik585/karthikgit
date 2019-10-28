160756d3c1ac6bfc89d3b7b79a68a50d32dd83d2
Karthik585 <56353567+Karthik585@users.noreply.github.com>
2019-10-28 19:23:26 +0530

    Update Kart.sql

--- Vdms/Kart.sql
+++ Vdms/Kart.sql
@@ -1,1 +1,1 @@
-CREATE TABLE persons1 ( person_id number(10) NOT NULL, first_name varchar2(50) NOT NULL, last_name varchar2(50) NOT NULL );
+CREATE TABLE health ( health_id number(10) NOT NULL, first_name varchar2(50) NOT NULL, last_name varchar2(50) NOT NULL );
