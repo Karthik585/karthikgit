c4d6817517d2615d7b9545548a02e71bff61ebd6
Karthik585 <56353567+Karthik585@users.noreply.github.com>
2019-11-05 12:25:34 +0530

    Update App.sql

--- Applicant/App.sql
+++ Applicant/App.sql
@@ -1,5 +1,5 @@
-CREATE TABLE passengers(  
-   cus_id INT NOT NULL AUTO_INCREMENT,  
+CREATE TABLE passengers
+(  cus_id INT NOT NULL AUTO_INCREMENT,  
    cus_firstname VARCHAR(100) NOT NULL,  
    cus_surname VARCHAR(100) NOT NULL,  
    PRIMARY KEY ( cus_id )  
